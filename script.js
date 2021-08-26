cb61fc1c8da15f25200eb3cc953b577e4be193ee
itsreallydrew <a.mclean2526@gmail.com>
2021-08-26 15:19:03 -0500

    initial pseudocode steps

---
+++ script.js
@@ -0,0 +1,34 @@
+/* REUSABLE ITEMS 
+// This section is to identify what actions/commands will be reused and can be written using a function:
+
+- The card being flipped to reveal the other side
+- Cycling to the next card or the previous card
+- Counting/Identifying which card in the stack the user is currently at
+    - have a counter
+- Adding a definition and a term to the deck
+- Create a class list for each new deck item that is added 
+- Update HTML with function 
+- Possibly add a quiz mode (stretch goal)
+- Store information in an object inside of an 
+
+*/
+
+// Pseudocode Functions
+
+// IF the user clicks on the card
+// THEN the card will flip to the other side and reveal the information
+
+// IF the user clicks an arrow
+// THEN the next card or the previous card will display
+
+// IF the user clicks the next arrow
+// THEN increment the counter by 1
+
+// IF the user clicks the previous arrow
+// THEN decrement the counter by 1
+
+// IF the user gets to the end of the stack
+// THEN start the count over -- count must start at 1
+
+// IF a new card is added
+// THEN increase the TOTAL by 1
