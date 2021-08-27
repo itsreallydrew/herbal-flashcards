e3a3597826d601d3aff1fd8b6d78a65c4aab8304
itsreallydrew <a.mclean2526@gmail.com>
2021-08-26 21:40:08 -0500

    Pseudocode and planning

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 /* REUSABLE ITEMS 
 // This section is to identify what actions/commands will be reused and can be written using a function:
 
-- The card being flipped to reveal the other side
+- Flipping the card to reveal the other side
 - Cycling to the next card or the previous card
 - Counting/Identifying which card in the stack the user is currently at
     - have a counter
 - Adding a definition and a term to the deck
-- Create a class list for each new deck item that is added 
 - Update HTML with function 
+- Have all of of the HTML clear out on each click and display the proper information
+- Have one part of the HTML have an #id of "term" and the other of "definition"
+- Create a class list for each new deck item that is added (stretch goal)
 - Possibly add a quiz mode (stretch goal)
-- Store information in an object inside of an 
+- Create a shuffle option 
+
 
 */
 
@@ -17,18 +20,50 @@
 
 // IF the user clicks on the card
 // THEN the card will flip to the other side and reveal the information
+function flipCard(params) {
+	// when the card is clicked it flips to the other side
+	// have the definition side be hidden (toggle) and on click/flip hide the term
+}
 
 // IF the user clicks an arrow
 // THEN the next card or the previous card will display
+function nextCard(params) {
+	// when the 'next' button is clicked the next card in the stack displays
+}
+
+function prevCard(params) {
+	// when the 'prev' button is clicked the previous card will display
+}
 
 // IF the user clicks the next arrow
 // THEN increment the counter by 1
+function incrementCount(params) {
+	// increase count by 1 per each 'next' arrow click
+}
 
 // IF the user clicks the previous arrow
 // THEN decrement the counter by 1
+function decrementCount(params) {
+	// decrease count by 1 per each 'previous' arrow click
+}
 
 // IF the user gets to the end of the stack
 // THEN start the count over -- count must start at 1
+function cardCarousel(params) {
+	//
+}
 
 // IF a new card is added
 // THEN increase the TOTAL by 1
+function cardTotal(params) {
+	// calculate total number of cards and increment by # cards added
+}
+
+// On each CLICK clear out DIV
+// THEN add the new PARAGRAPH item
+function clearCard(params) {
+	// clear the text of the card to allow for next piece of info to be displayed
+}
+
+// Total cards in stack = terms.length
+// currentCardCount = terms[i]
---
+++ script2.js
@@ -0,0 +1,38 @@
+// Create variable that holds all of the terms and definitions needed and place in this file.
+
+/* 
+This information will be referenced for the following:
+
+- flip card function
+- next card function
+- previous card function
+
+*/
+
+let terms = [
+	{
+		term: 'Drying',
+		definition:
+			'Removes excess fluid from tissue. Relieves conditions of dampness and swelling.',
+	},
+	{
+		term: 'Moistening',
+		definition:
+			'Increases moisture content of tissues. Lubricates and softens dry, brittle tissues.',
+	},
+	{
+		term: 'Balancing',
+		definition:
+			'Normalizes tissues, balancing moisture and solids within tissues.',
+	},
+	{
+		term: 'Nourishing',
+		definition:
+			'Provides essential nutrients that aid tissue healing, improving structure and function.',
+	},
+	{
+		term: 'Neutral',
+		definition:
+			'Herbs that are neither warm nor cool. They do not have a strong effect on circulation or metabolism.',
+	},
+];
